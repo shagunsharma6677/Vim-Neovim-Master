ffceb02039e1970f6873bc41b4aefa386de5bd90
shagunsharma6677 <shagunsharma6677@gmail.com>
2023-09-05 19:57:58 +0530

    day2

---
+++ Day-2/Day2.jsx
@@ -0,0 +1,52 @@
+const Sidebar = ({ categoryFilter, onPriceFilterChange }) => {
+  return (
+    <Accordion>
+      <AccordionItem marginTop="20px">
+        <h2>
+          <AccordionButton>
+            <Box as="span" flex="1" textAlign="left">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem
+              cum temporibus consequatur neque maxime eaque tenetur consectetur
+              sapiente esse? Labore tenetur magnam sapiente ipsam pariatur
+              itaqur, eucimus sint molestias ea doloremque praesentium autem
+              animi! Asperiores() qui iusto() cupiditate() there["a,"b."c"]
+              
+              
+              doloremque libero quas ab labore dignissimos rerum nobis dolor
+              
+              assumenda rem pariatur, consectetur distinctio nostrum, minus
+              suscipit cumque sequi architecto dolore! Suscipit alias voluptas
+              repellendus eum deleniti distinctio corrupti quaerat est dolores
+              sit doloremque, aliquam odio perspiciatis doloribus repellat amet!
+              Fugit non iusto voluptate, magni eius officia, nisi quisquam neque
+              commodi quasi minima veritatis magnam ad labore soluta rerum,
+              culpa aliquid Lorem ipsum dolor sit, amet consectetur adipisicing
+              elit. Autem
+            </Box>
+          </AccordionButton>
+        </h2>
+      </AccordionItem>
+      {/* Price */}
+      <AccordionItem marginTop="20px">
+        <h2>
+          <AccordionButton>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque
+            suscipit eum consequuntur non quis exercitationem et, eius,
+            repudiandae repellendus nesciunt voluptate iusto voluptatum
+            excepturi dolores ex velit pariatur! Fugiat quam expedita aspernatur
+            saepe facere veritatis tempore. Ullam natus aperiam, obcaecati
+            officiis atque explicabo vel numquam culpa. Eius facilis facere
+            asperiores quas eos amet, sapiente blanditiis nemo cum magnam
+            assumenda, ut distinctio consequuntur aut veniam eveniet! Distinctio
+            explicabo quae praesentium perferendis ad in, quisquam temporibus
+            est non quis placeat nam libero neque consequuntur eligendi
+            excepturi nemo ratione magni beatae enim ullam perspiciatis. Sunt
+            ipsum hic doloribus recusandae, consequuntur nisi. Ea, temporibus.
+          </AccordionButton>
+        </h2>
+      </AccordionItem>
+    </Accordion>
+  );
+};
+
+export default Sidebar;
